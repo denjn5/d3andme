4e41d06848d46cc174316eb90b8c9df17ab0cc08
denjn <denjn5@gmail.com>
2016-07-09 21:41:48 -0400

    updated to relative path

--- main.py
+++ main.py
@@ -1,35 +1,28 @@
 """
-main file that sets website 
+Set up website, point to index.html and supporting files. 
 
-TODO: root directory relies on a /LOC/ directory under C:.  Make this relative?
+Think MVC not directory structure!
+Orig path: '/LOC/Falcon/Falcon/static'
 """
 
-
 import bottle as web
+import os
 
 # Primary file
 @web.route('/')
 def index():
-    return web.static_file('index.html', root='/LOC/Falcon/Falcon/static')
+    path = os.path.dirname(os.path.realpath(__file__)) + '\static'
+    return web.static_file('index.html', root=path)
 
 
-# Primary file
-@web.route('/<filename>')
-def index(filename):
-    return web.static_file(filename, root='/LOC/Falcon/Falcon')
-
-# Think MVC not directory structure!
+# Supporting files
 @web.route('/static/<filename>')
 def index(filename):
-    return web.static_file(filename, root='/LOC/Falcon/Falcon/static')
+    path = os.path.dirname(os.path.realpath(__file__)) + '\static'
+    return web.static_file(filename, root=path)
 
-#@web.route('/hello')
-#@web.route('/hello/<name>')
 #@web.view('hello_template')
 #def hello(name='World'):
 #    return dict(name=name)
 
-
 web.run(host='localhost', port=8080, debug=True)
-
-
